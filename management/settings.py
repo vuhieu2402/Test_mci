dd11baaaa40d5e2ce2f74347e596e7113d825e13
vuhieu2402 <vuhieu24022002h@gmail.com>
2025-01-02 13:26:04 +0700

    update allow

--- management/settings.py
+++ management/settings.py
@@ -25,7 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+     'localhost', 
+    '127.0.0.1', 
+    'test-mci.onrender.com',  # Domain của ứng dụng trên Render
+]
 
 
 # Application definition
